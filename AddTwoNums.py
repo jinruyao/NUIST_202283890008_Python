0ba13da2d611485ea9e3db9477f15835399c5f4b
姚晋如 <yaojinru@yaojinrudeMacBook-Air.local>
2025-03-19 09:02:01 +0800

    Commit Version 3 AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,11 +1,18 @@
 # Add Two Numbers in Python
 # Author: Jinru Yao
-# Using the + Operator
+# Using a function
+
+# function to add two numbers
+def add(a, b):
+    # converting input to float and adding
+    result = float(a) + float(b)
+    return result
 
 # taking user input
 a = input("First Number: ")
 b = input("Second Number: ")
 
-# converting input to float and adding
-res = float(a) + float(b)
+# calling function
+res = add(a, b)
+print("The Answer is:")
 print(res)
